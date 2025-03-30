73b00e5e14a1d8ffa795dc3331bde545855b17c6
Aeron Asug <an0nymnx@outlook.com>
2025-03-30 16:55:42 -0600

    fix: temporarily commit changes to basic.ts; issue with import still present

--- examples/basic.ts
+++ examples/basic.ts
@@ -1,9 +1,9 @@
 import { textPrompt } from '@promptrix/core'
 
 (async () => {
-  const name = await textPrompt({
+  const name: string = await textPrompt({
     message: "Enter your name:",
-    validate: (input) => input.length > 2 || "Minimum 3 characters"
-  });
-  console.log(`Hello ${name}!`)
+    validate: (input: string) => input.length > 2 || "Minimum 3 characters"
+  })
+  console.log(`Hello, ${name}!`)
 })()
